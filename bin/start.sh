96ab82b4ff3b606ea45a834fc535626eaa3e04af
Vlad <yavl98@mail.ru>
2019-06-01 21:13:29 +0900

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 while true;
 do
-	cp infcroya.txt "logs/infcroya-$(date +"%d-%m-%y-%r").txt"
-	cp core "logs/core-$(date +"%d-%m-%y-%r")"
+	mv infcroya.txt "logs/infcroya-$(date +"%d-%m-%y-%r").txt"
+	mv core "logs/core-$(date +"%d-%m-%y-%r")"
 	./server
 done
