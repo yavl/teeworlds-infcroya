c62058665730fda06366c174c2996fd795f8c3d9
Vlad <yavl98@mail.ru>
2019-08-06 17:19:34 +0900

    Add infc_junglewell.lua

---
+++ bin/maps/infc_junglewell.lua
@@ -0,0 +1,114 @@
+-- shrinking from 6500 to 800 (with shrink speed = 1) takes 114 seconds
+-- why: 6500 - 800 = 5700; 5700 / TickSpeed = 114; where TickSpeed = 50
+default_radius = 6500
+min_radius = 800
+circle_shrink_speed = 1
+default_inf_radius = 300
+timelimit = 3
+
+circle_positions = {}
+
+inf_circle_positions = {
+	-- x, y, radius
+	48, 94, 350,
+	141, 143, 150
+}
+
+math.randomseed(os.time())
+
+function infc_init()
+	-- infc_init() is called 10 secs after round start
+	-- hardcode passed C/C++ values here (e.g for tests)
+	-- passed values: infc_num_players
+	case = math.random(1, 10) -- 10 different cases
+	if case == 1 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			114, 125, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 2 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			144, 120, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 3 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			45, 126, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 4 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			75, 125, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 5 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			128, 82, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 6 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			121, 59, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 7 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			183, 71, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 8 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			172, 21, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 9 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			96, 108, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+	
+	if case == 10 then
+		circle_positions = { 
+			-- x, y, radius, minradius
+			31, 53, default_radius, min_radius, circle_shrink_speed,
+		}
+	end
+end
+
+-- Try not to modify functions below
+function infc_get_circle_pos()
+	return circle_positions
+end
+
+function infc_get_inf_circle_pos()
+	return inf_circle_positions
+end
+
+function infc_get_timelimit()
+	return timelimit
+end
+
+function infc_get_circle_min_radius()
+	return min_radius
+end
+
+function infc_get_circle_shrink_speed()
+	return circle_shrink_speed
+end
